fa7a127c5ffa0a041faafd7b328eeb5300f1d7a4
alxmart <alxmartinez@gmail.com>
2024-01-28 09:46:37 -0300

    Added fun clicar and finishing layout.

--- app/src/main/java/com/luizafmartinez/m01_foiclicado/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/m01_foiclicado/MainActivity.kt
@@ -2,10 +2,16 @@ package com.luizafmartinez.m01_foiclicado
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clicar(view: View) {
+        println("O botão foi clicado!")
     }
 }
